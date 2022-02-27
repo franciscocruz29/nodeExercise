9670a260a9eecd39d77b6dd2c6fb0f3dc4f7cf1f
franciscocruz29 <franciscohuertas25@gmail.com>
2022-02-27 17:57:55 -0500

    Checked name length

--- names.js
+++ names.js
@@ -7,6 +7,8 @@ for (let count = 0; count < 999; count++) {
   strNames += faker.name.findName() + ', ';
 }
 
+// console.log(strNames.split(',').length);
+
 fs.writeFile('names.txt', strNames, function(err) { 
   if(err) return console.log(err); else console.log('file saved')
 });
