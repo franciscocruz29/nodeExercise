db8281dd1793540e9e31ae3af512ef95b6754b55
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-02 15:00:14 -0500

    Add function createFakeNames

--- names.js
+++ names.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const { faker } = require('@faker-js/faker');
 
-let strNames = '';
+function createFakeNames() {
+  let strNames = '';
 
-for (let count = 0; count < 999; count++) {
-  strNames += faker.name.findName() + ', ';
+  for (let count = 0; count < 999; count++) {
+    strNames += faker.name.findName() + ', ';
+  }
+  return strNames
 }
 
-// console.log(strNames.split(',').length);
-
-fs.writeFile('names.txt', strNames, function(err) { 
+fs.writeFile('names.txt', createFakeNames(), function(err) { 
   if(err) return console.log(err); else console.log('file saved')
 });
